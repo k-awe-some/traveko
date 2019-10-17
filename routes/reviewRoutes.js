2a8e38f34bd2df65b4b2bcda80a3bcaff77ca253
kay <kaynguyen.dev@gmail.com>
2019-10-17 15:40:56 -0400

    refactored route handlers code

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,31 +5,29 @@ module.exports = app => {
     fs.readFileSync(`${__dirname}/../dev-data/data/reviews.json`)
   );
 
-  app.get("/api/v1/reviews", (req, res) => {
+  app.get("/api/v1/reviews", (req, res) =>
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
       results: reviews.length,
       data: { reviews }
-    });
-  });
+    })
+  );
 
   app.get("/api/v1/reviews/:id", (req, res) => {
     const id = req.params.id;
     const review = reviews.find(review => review._id === id);
 
-    if (!review) {
-      return res.status(404).json({
-        status: "failure",
-        requestedAt: req.requestTime,
-        message: "Invalid ID"
-      });
-    }
-
-    res.status(200).json({
-      status: "success",
-      requestedAt: req.requestTime,
-      data: { review }
-    });
+    review
+      ? res.status(200).json({
+          status: "success",
+          requestedAt: req.requestTime,
+          data: { review }
+        })
+      : res.status(404).json({
+          status: "failure",
+          requestedAt: req.requestTime,
+          message: "Invalid ID"
+        });
   });
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,31 +5,29 @@ module.exports = app => {
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
   );
 
-  app.get("/api/v1/tours", (req, res) => {
+  app.get("/api/v1/tours", (req, res) =>
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
       results: tours.length,
       data: { tours }
-    });
-  });
+    })
+  );
 
   app.get("/api/v1/tours/:id", (req, res) => {
     const id = Number(req.params.id);
     const tour = tours.find(tour => tour.id === id);
 
-    if (!tour) {
-      return res.status(404).json({
-        status: "failure",
-        requestedAt: req.requestTime,
-        message: "Invalid ID"
-      });
-    }
-
-    res.status(200).json({
-      status: "success",
-      requestedAt: req.requestTime,
-      data: { tour }
-    });
+    tour
+      ? res.status(200).json({
+          status: "success",
+          requestedAt: req.requestTime,
+          data: { tour }
+        })
+      : res.status(404).json({
+          status: "failure",
+          requestedAt: req.requestTime,
+          message: "Invalid ID"
+        });
   });
 };
