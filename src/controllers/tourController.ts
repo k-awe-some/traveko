aa9d222a488030c99dabf38f8bc8af106bbe8e8f
kay <kaynguyen.dev@gmail.com>
2019-10-19 15:21:43 -0400

    created Tour model

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -1,44 +1,15 @@
 import fs from "fs";
-import { NextFunction, Request, Response } from "express";
-
-const tours = JSON.parse(
-  fs.readFileSync(
-    `${__dirname}/../../dev-data/data/tours-simple.json`,
-    encodeURI("")
-  )
-);
-
-// middlewares
-export const checkTourID = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  val: number
-) => {
-  console.log(`Tour id is ${val}`);
-
-  Number(req.params.id) > tours.length
-    ? res.status(404).json({
-        status: "failure",
-        message: "Invalid tour ID"
-      })
-    : next();
-};
+import { Request, Response } from "express";
+import Tour from "../models/tourModel";
 
 // controllers
 export const getAllTours = (req: Request, res: Response) =>
   res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: { tours }
+    status: "success"
   });
 
 export const getTour = (req: Request, res: Response) => {
-  const id: number = Number(req.params.id);
-  const tour: object = tours.find((tour: any) => tour.id === id);
-
   res.status(200).json({
-    status: "success",
-    data: { tour }
+    status: "success"
   });
 };
--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -1,11 +1,7 @@
-import {
-  checkTourID,
-  getAllTours,
-  getTour
-} from "../controllers/tourController";
+import { getAllTours, getTour } from "../controllers/tourController";
 
 const tourRoutes = (app: any) => {
-  app.param("id", checkTourID);
+  // app.param("id", checkTourID); // no longer needed but was a good example on how to run this middleware in this case
   app.get("/api/v1/tours", getAllTours);
   app.get("/api/v1/tours/:id", getTour);
 };
