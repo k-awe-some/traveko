9c9dc8802d1bad882f1908c229eafd6a36057014
kay <kaynguyen.dev@gmail.com>
2019-10-19 16:57:32 -0400

    improved Tour model

--- src/models/tourModel.ts
+++ src/models/tourModel.ts
@@ -4,16 +4,54 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "A tour must have a name"],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A tour must specify a duration"]
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must specify a group size"]
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must specify a difficulty level"]
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, "A tour must have a price"]
-  }
+  },
+  discountPrice: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a summary"]
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a description"]
+  },
+  imageCover: {
+    type: String, // name of image
+    required: [true, "A tour must have a cover image"]
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
