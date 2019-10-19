d442b872fea6b38984ab8ec06a5a87d730e04d3e
kay <kaynguyen.dev@gmail.com>
2019-10-18 22:37:24 -0400

    added middleware for tour routes: checkTourID

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 const tours = JSON.parse(
   fs.readFileSync(
@@ -8,6 +8,24 @@ const tours = JSON.parse(
   )
 );
 
+// middlewares
+export const checkTourID = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  val: number
+) => {
+  console.log(`Tour id is ${val}`);
+
+  Number(req.params.id) > tours.length
+    ? res.status(404).json({
+        status: "failure",
+        message: "Invalid tour ID"
+      })
+    : next();
+};
+
+// controllers
 export const getAllTours = (req: Request, res: Response) =>
   res.status(200).json({
     status: "success",
@@ -19,13 +37,8 @@ export const getTour = (req: Request, res: Response) => {
   const id: number = Number(req.params.id);
   const tour: object = tours.find((tour: any) => tour.id === id);
 
-  tour
-    ? res.status(200).json({
-        status: "success",
-        data: { tour }
-      })
-    : res.status(404).json({
-        status: "failure",
-        message: "Invalid tour ID"
-      });
+  res.status(200).json({
+    status: "success",
+    data: { tour }
+  });
 };
--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -1,6 +1,11 @@
-import { getAllTours, getTour } from "../controllers/tourController";
+import {
+  checkTourID,
+  getAllTours,
+  getTour
+} from "../controllers/tourController";
 
 const tourRoutes = (app: any) => {
+  app.param("id", checkTourID);
   app.get("/api/v1/tours", getAllTours);
   app.get("/api/v1/tours/:id", getTour);
 };
