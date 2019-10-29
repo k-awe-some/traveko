4e4bebc5e59d9d6702beebac083841315e8eef8c
kay <kaynguyen.dev@gmail.com>
2019-10-28 21:49:51 -0400

    signed new users up

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,24 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/userModel";
 import catchAsync from "../utils/catchAsync";
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    });
 
     res.status(201).json({
       status: "success",
+      token,
       data: { user: newUser }
     });
   }
