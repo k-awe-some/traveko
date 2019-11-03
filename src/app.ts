d3dae4f79113218d7e1ffff593f99f060f874c68
kay <kaynguyen.dev@gmail.com>
2019-11-03 11:33:05 -0500

    enhanced security: setting HTTP headers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 
 import reviewRoutes from "./routes/reviewRoutes";
 import tourRoutes from "./routes/tourRoutes";
@@ -10,17 +11,27 @@ import unhandledRoutes from "./routes/unhandledRoutes";
 const app = express();
 
 /*** GLOBAL MIDDLEWARES ***/
+// set security HTTP headers
+app.use(helmet());
+
+// development log in
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// limit requests from same IP
 const limiter = rateLimit({
   max: 500,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour."
 });
 app.use("/api", limiter);
-app.use(express.json());
+
+// (body-parser) read data from body to req.body
+app.use(express.json({ limit: "10kb" }));
+
+// serve static fiels
+// app.use(express.static(`${__dirname}/public`))
 
 /*** ROUTE HANLDERS ***/
 reviewRoutes(app);
